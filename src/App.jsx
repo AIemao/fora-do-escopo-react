63b74cc5175c0da440d82a6579a4c7ff7d48fcd7
Celio Junior <celioju@hotmail.com>
2023-08-27 10:19:42 -0300

    Beta1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Form from "./pages/Form";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1> hello</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <nav id="menu">
+      <h1>SERVIÇO FORA DO ESCOPO</h1>
+    </nav>
+  );
+}
