fc94aeba6c323cbcc896e9f1734dd2ea9946c298
LuizLimaDev <devluiz.lima@gmail.com>
2025-03-04 16:12:10 -0300

    style: general style

--- src/components/ModalConfigInfo/ModalConfigInfo.jsx
+++ src/components/ModalConfigInfo/ModalConfigInfo.jsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import style from "./ModalConfigInfo.module.css"
 
 export default function ModalConfigInfo({
   modalVisible,
@@ -33,172 +34,217 @@ export default function ModalConfigInfo({
       {/* Conteúdo do modal */}
       <h2>Preencha as perguntas:</h2>
       <form>
-        <div className="pergunta">
-          <p>A instalação será realizada com Backup ou Base Zerada?</p>
+        <div>
+          <strong>A instalação será realizada com Backup ou Base Zerada?</strong>
 
-          <input
-            type="radio"
-            name="backup"
-            value="Backup"
-            onChange={(e) => setBackup(e.target.value)}
-          />
-          <label>Backup</label>
-          <input
-            type="radio"
-            name="backup"
-            value="Base Zerada"
-            onChange={(e) => setBackup(e.target.value)}
-          />
-          <label>Base Zerada</label>
-          <input
-            type="radio"
-            name="backup"
-            value="Não será feita instalação de SERVIDOR"
-            onChange={(e) => setBackup(e.target.value)}
-          />
-          <label>Não será feita instalação de SERVIDOR</label>
+          <div className={style.container_question}>
+            <label>
+              <input
+                type="radio"
+                name="backup"
+                value="Backup"
+                onChange={(e) => setBackup(e.target.value)}
+              />
+              Backup
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="backup"
+                value="Base Zerada"
+                onChange={(e) => setBackup(e.target.value)}
+              />
+              Base Zerada
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="backup"
+                value="Não será feita instalação de SERVIDOR"
+                onChange={(e) => setBackup(e.target.value)}
+              />
+              Não será feita instalação de SERVIDOR
+            </label>
+          </div>
         </div>
 
-        <div className="pergunta">
-          <p>A loja utiliza Mobile?</p>
+        <div >
+          <strong>A loja utiliza Mobile?</strong>
 
-          <input
-            type="radio"
-            name="mobile"
-            value="Sim"
-            onChange={(e) => setMobile(e.target.value)}
-          />
-          <label>Sim</label>
-          <input
-            type="radio"
-            name="mobile"
-            value="Não"
-            onChange={(e) => setMobile(e.target.value)}
-          />
-          <label>Não</label>
+          <div className={style.container_question}>
+            <label>
+              <input
+                type="radio"
+                name="mobile"
+                value="Sim"
+                onChange={(e) => setMobile(e.target.value)}
+              />
+              Sim
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                name="mobile"
+                value="Não"
+                onChange={(e) => setMobile(e.target.value)}
+              />
+              Não
+            </label>
+          </div>
         </div>
 
-        <div className="pergunta">
-          <p>A loja utiliza Auto Atendimento ONE?</p>
+        <div >
+          <strong>A loja utiliza Auto Atendimento ONE?</strong>
 
-          <input
-            type="radio"
-            name="aa"
-            value="Sim"
-            onChange={(e) => setAA(e.target.value)}
-          />
-          <label>Sim</label>
-          <input
-            type="radio"
-            name="aa"
-            value="Não"
-            onChange={(e) => setAA(e.target.value)}
-          />
-          <label>Não</label>
+          <div className={style.container_question}>
+            <div>
+              <input
+                type="radio"
+                name="aa"
+                value="Sim"
+                onChange={(e) => setAA(e.target.value)}
+              />
+              <label>Sim</label>
+            </div>
+            <div>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="aa"
+                value="Não"
+                onChange={(e) => setAA(e.target.value)}
+              />
+              <label>Não</label>
+            </div>
+          </div>
         </div>
 
-        <div className="pergunta">
-          <p>Loja está parada?</p>
+        <div className={style.container_question}>
+          <strong>Loja está parada?</strong>
 
-          <input
-            type="radio"
-            name="lojaParada"
-            value="Sim"
-            onChange={(e) => setLojaParada(e.target.value)}
-          />
-          <label>Sim</label>
-          <input
-            type="radio"
-            name="lojaParada"
-            value="Não"
-            onChange={(e) => setLojaParada(e.target.value)}
-          />
-          <label>Não</label>
+          <div className={style.container_question}>
+            <div>
+              <input
+                type="radio"
+                name="lojaParada"
+                value="Sim"
+                onChange={(e) => setLojaParada(e.target.value)}
+              />
+              <label>Sim</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="lojaParada"
+                value="Não"
+                onChange={(e) => setLojaParada(e.target.value)}
+              />
+              <label>Não</label>
+            </div>
+
+          </div>
+
         </div>
 
-        <div className="pergunta">
-          <p>Quantos PDVs a loja possui?</p>
+        <div className={style.container_question}>
+          <strong>Quantos PDVs a loja possui?</strong>
           <input
             type="number"
             name="quantidadePDVs"
             min="0"
             onChange={(e) => setQuantidadePDVs(e.target.value)}
             placeholder="Informe a quantidade"
+            className={style.input_number}
           />
         </div>
 
-        <div className="pergunta">
-          <p>Quantos PDVs serão instalados?</p>
+        <div className={style.container_question}>
+          <strong>Quantos PDVs serão instalados?</strong>
           <input
             type="number"
             name="quantidadePDVsInstalados"
             min="0"
             onChange={(e) => setQuantidadePDVsInstalados(e.target.value)}
             placeholder="Informe a quantidade"
+            className={style.input_number}
           />
         </div>
 
-        <div className="pergunta">
-          <p>Qual o número do PDV que será instalado?</p>
+        <div className={style.container_question}>
+          <strong>Qual o número do PDV que será instalado?</strong>
           <input
             type="text"
             name="numeroPDVInstalado"
             onChange={(e) => setNumeroPDVInstalado(e.target.value)}
             placeholder="Informe o número"
+            className={style.input_number}
           />
         </div>
 
-        <div className="pergunta">
-          <p>Qual é o emissor de cupom utilizado (S@T, NFCE, ECF)?</p>
-          <input
-            type="radio"
-            name="emissorCupom"
-            value="S@T"
-            onChange={(e) => setEmissorCupom(e.target.value)}
-          />
-          <label>S@T</label>
-          <input
-            type="radio"
-            name="emissorCupom"
-            value="NFCE"
-            onChange={(e) => setEmissorCupom(e.target.value)}
-          />
-          <label>NFCE</label>
-          <input
-            type="radio"
-            name="emissorCupom"
-            value="ECF"
-            onChange={(e) => setEmissorCupom(e.target.value)}
-          />
-          <label>ECF</label>
+        <div className={style.container_question}>
+          <strong>Qual é o emissor de cupom utilizado (S@T, NFCE, ECF)?</strong>
+          <div>
+            <input
+              type="radio"
+              name="emissorCupom"
+              value="S@T"
+              onChange={(e) => setEmissorCupom(e.target.value)}
+            />
+            <label>S@T</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="emissorCupom"
+              value="NFCE"
+              onChange={(e) => setEmissorCupom(e.target.value)}
+            />
+            <label>NFC-e</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="emissorCupom"
+              value="ECF"
+              onChange={(e) => setEmissorCupom(e.target.value)}
+            />
+            <label>ECF</label>
+          </div>
         </div>
 
-        <div className="pergunta">
-          <p>Qual é a marca e modelo do emissor de cupom (DANFE)?</p>
+        <div className={style.container_question}>
+          <strong>Qual é a marca e modelo do emissor de cupom (DANFE)?</strong>
           <input
             type="text"
             name="marcaModeloEmissorCupom"
             onChange={(e) => setMarcaModeloEmissorCupom(e.target.value)}
             placeholder="Informe a marca e modelo"
+            className={style.input_number}
           />
         </div>
 
-        <div className="pergunta">
-          <p>Impressoras Remotas?</p>
-          <input
-            type="radio"
-            name="impressorasRemotas"
-            value="Sim"
-            onChange={(e) => setImpressorasRemotas(e.target.value)}
-          />
-          <label>Sim</label>
-          <input
-            type="radio"
-            name="impressorasRemotas"
-            value="Não"
-            onChange={(e) => setImpressorasRemotas(e.target.value)}
-          />
-          <label>Não</label>
+        <div className={style.container_question}>
+          <strong>Impressoras Remotas?</strong>
+          <div>
+            <input
+              type="radio"
+              name="impressorasRemotas"
+              value="Sim"
+              onChange={(e) => setImpressorasRemotas(e.target.value)}
+            />
+            <label>Sim</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="impressorasRemotas"
+              value="Não"
+              onChange={(e) => setImpressorasRemotas(e.target.value)}
+            />
+            <label>Não</label>
+          </div>
           <br />
           {impressorasRemotas === "Sim" && (
             <input
@@ -210,22 +256,26 @@ export default function ModalConfigInfo({
           )}
         </div>
 
-        <div className="pergunta">
-          <p>Loja tem PIN PAD?</p>
-          <input
-            type="radio"
-            name="pinPad"
-            value="Sim"
-            onChange={(e) => setPinPad(e.target.value)}
-          />
-          <label>Sim</label>
-          <input
-            type="radio"
-            name="pinPad"
-            value="Não"
-            onChange={(e) => setPinPad(e.target.value)}
-          />
-          <label>Não</label>
+        <div className={style.container_question}>
+          <strong>Loja tem PIN PAD?</strong>
+          <div>
+            <input
+              type="radio"
+              name="pinPad"
+              value="Sim"
+              onChange={(e) => setPinPad(e.target.value)}
+            />
+            <label>Sim</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="pinPad"
+              value="Não"
+              onChange={(e) => setPinPad(e.target.value)}
+            />
+            <label>Não</label>
+          </div>
           <br />
           {pinPad === "Sim" && (
             <input
@@ -237,23 +287,27 @@ export default function ModalConfigInfo({
           )}
         </div>
 
-        <div className="pergunta">
-          <p>Existe mais algum equipamento conectado à máquina?</p>
-          <input
-            type="radio"
-            name="equipamentoConectado"
-            value="Sim"
-            onChange={(e) => setEquipamentoConectado(e.target.value)}
-          />
-          <label>Sim</label>
-          <input
-            type="radio"
-            name="equipamentoConectado"
-            value="Não"
-            onChange={(e) => setEquipamentoConectado(e.target.value)}
-          />
-          <label>Não</label>
-          <br />
+        <div className={style.container_question}>
+          <strong>Existe mais algum equipamento conectado à máquina?</strong>
+          <div>
+            <input
+              type="radio"
+              name="equipamentoConectado"
+              value="Sim"
+              onChange={(e) => setEquipamentoConectado(e.target.value)}
+            />
+            <label>Sim</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="equipamentoConectado"
+              value="Não"
+              onChange={(e) => setEquipamentoConectado(e.target.value)}
+            />
+            <label>Não</label>
+          </div>
+
           {equipamentoConectado === "Sim" && (
             <input
               type="text"
@@ -265,14 +319,14 @@ export default function ModalConfigInfo({
             />
           )}
         </div>
-        <br />
 
-        <button type="button" onClick={handleCopyAnswers}>
+
+        <button type="button" onClick={handleCopyAnswers} className={style.button_copy}>
           Copiar Respostas
         </button>
         <br />
 
-        <button onClick={() => setModalVisible(false)}>Fechar</button>
+        <button onClick={() => setModalVisible(false)} className={style.button_close}>Fechar</button>
       </form>
     </Modal>
   );
